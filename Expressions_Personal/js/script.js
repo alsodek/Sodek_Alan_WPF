f0538d51ffa4b3df2184a878c2391a2192ce0bbf
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 16:16:16 -0600

    mpg variable works

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,13 +3,13 @@
 //Expressions-Personal
 
 //How much will you spend on fuel on a road trip.
-var miles = prompt("We are calculating how much you will send on fuel for a road trip. \nHow many miles will the trip be?"); //
-//var mpg = prompt(); //
+var miles = prompt("We are calculating how much you will send on fuel for a road trip. \nHow many miles will the trip be?"); //Ask how many miles the road trip is.
+var mpg = prompt("How many miles per gallon does your car get?"); //Ask how many miles per gallon their car gets.
 //var price = prompt(); //
 
 //var  = miles/mpg; //Divide the miles by the mpg.
  
 //var = ; //Multiply by the price.
 
-console.log(miles);
+console.log(mpg);
 
