8c94d62a99375f080587fa8285c785da8ae036d2
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 17:12:05 -0600

    test0 works fine

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,7 +4,7 @@
 
 //Find the average of 5 test grades.
 
-var test0 = prompt(); //Ask for the first test grade.
+var test0 = prompt("We will find the average of five test grades. \nWhat is the first test grade?"); //Ask for the first test grade.
 
 var test1 = prompt(); //Ask for the second test grade.
 
@@ -15,3 +15,5 @@ var test3 = prompt(); //Ask for the fourth test grade.
 var test4 = prompt(); //Ask for the fifth test grade.
 
 var tests = [test0, test1, test2, test3, test4]; //The array.
+
+console.log(test0);
