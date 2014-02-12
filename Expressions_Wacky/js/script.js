bf348ba069c23c250e30e2319ac22ab2a1127da6
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 17:08:19 -0600

    set up variables

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,6 +4,14 @@
 
 //Find the average of 5 test grades.
 
+var test0 = prompt();
 
+var test1 = prompt();
+
+var test2 = prompt();
+
+var test3 = prompt();
+
+var test4 = prompt();
 
 var tests = [test0, test1, test2, test3, test4]; //The array.
