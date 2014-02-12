b6be123b0084499fe9c7d6ee192e15623d97ad6f
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 16:50:22 -0600

    length variable works

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,12 @@
 //Alan Sodek
 //February 12, 2014
 //Expressions
+
+//I work for a landscape nursery and this is a problem that i frequently run into.
+//How much soil will it take to fill a garden box?
+
+var length = prompt("What is the length of the box?"); //Ask for the lenght of the box.
+
+
+
+console.log(length);
