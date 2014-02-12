3775be89299a35419cd2fe181da266c2d274d0cb
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 16:21:39 -0600

    finalMiles variable works

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,11 +5,11 @@
 //How much will you spend on fuel on a road trip.
 var miles = prompt("We are calculating how much you will send on fuel for a road trip. \nHow many miles will the trip be?"); //Ask how many miles the road trip is.
 var mpg = prompt("How many miles per gallon does your car get?"); //Ask how many miles per gallon their car gets.
-//var price = prompt(); //
+var price = prompt("What is the current price of fuel (per gallon)?"); //Ask the current price of fuel.
 
-//var  = miles/mpg; //Divide the miles by the mpg.
+var finalMiles = miles/mpg; //Divide the miles by the mpg.
  
-//var = ; //Multiply by the price.
+//var  = ; //Multiply by the price.
 
-console.log(mpg);
+console.log(finalMiles);
 
