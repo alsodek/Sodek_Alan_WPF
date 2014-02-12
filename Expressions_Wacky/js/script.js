77b0f2696f9c3819d5d404d9e0f14ee686b2763b
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 17:09:12 -0600

    added comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,14 +4,14 @@
 
 //Find the average of 5 test grades.
 
-var test0 = prompt();
+var test0 = prompt(); //Ask for the first test grade.
 
-var test1 = prompt();
+var test1 = prompt(); //Ask for the second test grade.
 
-var test2 = prompt();
+var test2 = prompt(); //Ask for the third test grade.
 
-var test3 = prompt();
+var test3 = prompt(); //Ask for the fourth test grade.
 
-var test4 = prompt();
+var test4 = prompt(); //Ask for the fifth test grade.
 
 var tests = [test0, test1, test2, test3, test4]; //The array.
