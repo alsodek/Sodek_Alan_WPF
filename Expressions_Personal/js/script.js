f6c1d0bfd8ebe97d328d67632dfe5f1da6acb715
alsodek <alansodek@alans-mbp.att.net>
2014-02-12 16:03:49 -0600

    initial setup on personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,15 @@
 //Alan Sodek
 //February 12, 2014
-//Expressions
+//Expressions-Personal
+
+//How much will you spend on fuel on a road trip.
+var miles = prompt(); //
+var mpg = prompt(); //
+var price = prompt(); //
+
+var = ; //Divide the miles by the mpg.
+ 
+var = ; //Multiply by the price.
+
+console.log();
+
