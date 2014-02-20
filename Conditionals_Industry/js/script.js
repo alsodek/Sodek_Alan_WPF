77c2d27a49f11bbc8243ee915ae23103d7a36c78
alsodek <alansodek@alans-mbp.att.net>
2014-02-19 20:51:20 -0600

    initial set up

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,12 @@
-alert("This is an alert");
+//Alan Sodek
+//February 19, 2014
+//Conditionals
+
+
+
+
+
+if(bills === ""){
+	//If the prompt is left blank let the user know.
+	console.log("You forgot to input something!");
+}
