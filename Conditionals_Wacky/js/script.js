ef909d879040b3b0e53cb2e8caee28ffcc6d4f6d
alsodek <alansodek@alans-mbp.att.net>
2014-02-19 21:09:51 -0600

    created variables and else if outline

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,7 +2,15 @@
 //February 19, 2014
 //Conditionals
 
+//What activity to do depending on what time of day it is.
 
+//Ask what time of day it is.
+var time = prompt("");
+
+//Ask for a.m. or p.m..
+var day = prompt("");
+
+//
 
 
 
@@ -10,4 +18,13 @@
 if(image === ""){
 	//If the prompt is left blank let the user know.
 	console.log("You forgot to input something!");
+}else if(){
+	//
+	
+}else if(){
+	//
+	
+}else if(){
+	//
+	
 }
