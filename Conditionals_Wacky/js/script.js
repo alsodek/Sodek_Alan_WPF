626a0a470f95d15ebd8133f0f8706b813abd0247
alsodek <alansodek@alans-mbp.att.net>
2014-02-19 21:06:06 -0600

    initial setup

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,13 @@
-alert("This is an alert");
+//Alan Sodek
+//February 19, 2014
+//Conditionals
+
+
+
+
+
+//This is if the user enters an empty prompt.
+if(image === ""){
+	//If the prompt is left blank let the user know.
+	console.log("You forgot to input something!");
+}
