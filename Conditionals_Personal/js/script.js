c27eac0858fbd1ba3a1cda9564f819798d2dcd0c
alsodek <alansodek@alans-mbp.att.net>
2014-02-19 20:17:43 -0600

    set up variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,17 @@
-alert("This is an alert");
+//Alan Sodek
+//February 19, 2014
+//Conditionals
+
+//Can you afford the car you want?
+
+//Cost of Car.
+var car = 
+
+//How much money you have saved.
+var savings = 
+
+//How much income you have per month.
+var income = 
+
+//How much you have to pay for bills per month.
+var bills = 
