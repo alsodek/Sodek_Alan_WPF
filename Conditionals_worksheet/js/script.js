9005239d2f50fa3a300811ef9fb3531b21ebda1a
alsodek <alansodek@alans-mbp.att.net>
2014-02-19 18:59:55 -0600

    initial setup of problem 2

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -23,3 +23,31 @@ if(unit === "F"){
 	//Print out the result in Fahrenheit.
 	console.log("The temperature is " + cel + " degrees Fahrenheit.");
 }
+
+
+
+//Check the Login
+//Check to see if the username and password are correct.
+
+//Ask for the username.
+var username = prompt("Enter your username:");
+
+//Ask for the password.
+var password = prompt("Enter your password:");
+
+//Correct username
+var correctUsername = "";
+
+//Correct password
+var correctPassword = "";
+
+if(username === correctUsername && password === correctPassword){
+	//If the username and password are correct tell the user.
+	console.log("Welcome, " + username + "!");
+}else if(username != correctUsername){
+	//If the username isn't correct tell the user.
+	console.log("User not found. Try again.");
+}else{
+	//If the password isn't correct tell the user.
+	console.log("Password does not match our records.");
+}
