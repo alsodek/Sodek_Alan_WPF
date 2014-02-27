45e55a7ae56a5537f7c3aed0d1904a2a92ee1255
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 11:36:14 -0600

    finished first problem

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,21 @@
+// Alan Sodek 
+// February 27, 2014
+// Functions Worksheet
+
+// Circumference
+
+function calcCircumference(radius){
+	// Multiply radius by 2 by pi (3.14159
+	var circumference = 2 * 3.14159 * radius;
+	return circumference; //Return variable circumference to total
+}
+
+var total = calcCircumference(5);
+
+console.log("The circumference of the circle is " + total + ".");
+
+
+
+//Stung!
+
+
