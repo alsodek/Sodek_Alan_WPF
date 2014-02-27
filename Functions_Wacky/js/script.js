a5496fe600edb40cceb30a6b4996b77cdcc6e81c
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 13:23:57 -0600

    set up tree var and if else statement

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,18 @@
 // Alan Sodek
 // February 27, 2014
 // Functions Wacky
+
+// How many trees can you fit in your front yard?
+
+// Tree takes up 10 sq. ft.
+var tree = 10;
+
+
+
+if(){
+	//
+	
+}else{
+	//
+	
+}
