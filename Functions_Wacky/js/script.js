cde5462c391fefe7b235c29ffd5a8bfbf789498d
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 13:46:39 -0600

    fixed several spelling errors and added "final" variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,19 +4,19 @@
 
 // How many trees can you fit in your front yard?
 
-function findTrees(lengh, width){
+function findTrees(length, width){
 	// Tree takes up 10 sq. ft.
 	var tree = 10;
 	// Calculate area by multiplying length by width.
 	var area = length * width;
 	// Calculate how many trees can fit by dividing area by trees.
-	var totalTrees = area/trees;
+	var totalTrees = area/tree;
 	// Return results to total variable.
 	return totalTrees;
 }
 
 var total = findTrees(100, 100);
 
-total = (totalTrees > 0 ) ? "You can put in " + totalTrees + " trees." : "You don't have enough room for a tree!";
+final = (total > 0 ) ? "You can put in " + total + " trees." : "You don't have enough room for a tree!";
 
-console.log(total);
+console.log(final);
