a1ac3634c686d79032086b8a493fb315ac3a5234
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 11:43:47 -0600

    finished problem 2

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -5,7 +5,7 @@
 // Circumference
 
 function calcCircumference(radius){
-	// Multiply radius by 2 by pi (3.14159
+	// Multiply radius by 2 by pi (3.14159)
 	var circumference = 2 * 3.14159 * radius;
 	return circumference; //Return variable circumference to total
 }
@@ -18,4 +18,13 @@ console.log("The circumference of the circle is " + total + ".");
 
 //Stung!
 
+function beeStings(weight){
+	// Multiply weight by bee stings (8.666666667)
+	var stings= weight * 8.666666667;
+	return stings; //Return variable stings to total
+}
+
+var total = beeStings(100);
+
+console.log("It takes " + total + " bee stings to kill this animal.");
 
