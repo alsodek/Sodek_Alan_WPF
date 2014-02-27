ad2ffbd388c54aac9ce381412e1698c0b018727c
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 14:02:09 -0600

    created else if statement

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,11 +2,20 @@
 // February 27, 2014
 // Functions Industry
 
-// 
+// Will the picture fit into the space?
 
 var space = function(length, width){
+	// Calculate the area by multiplying length by width.
+	var area = length * width;
+	// 
 	
+}
 
+if(){
+
+}else if(){
+
+}else{
 
 }
 
