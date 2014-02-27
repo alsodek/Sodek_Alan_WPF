1cd68e0703c6597ef5a6fd7daa8f9bcb880ca3e4
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 13:37:59 -0600

    added function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,13 +4,16 @@
 
 // How many trees can you fit in your front yard?
 
-// Tree takes up 10 sq. ft.
-var tree = 10;
+function findTrees(lengh, width){
+	// Tree takes up 10 sq. ft.
+	var tree = 10;
+	// Calculate area by multiplying length by width.
+	var area = length * width;
+	// Calculate how many trees can fit by dividing area by trees.
+	var totalTrees = area/trees;
+}
 
-var length = 100;
-var width = 100;
-var area = length * width;
-var totalTrees = area/trees;
+
 
 total = (totalTrees > 0 ) ? "You can put in " + totalTrees + " trees." : "You don't have enough room for a tree!";
 
