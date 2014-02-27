a2ac2ea85b8c76f717b18737d1bd6046b3be4f7f
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 14:29:01 -0600

    finished problem

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,20 +4,26 @@
 
 // Will the picture fit into the space?
 
-var space = function(length, width){
+var space = function(){
+	// Ask the user for the length.
+	var length = prompt("What is the length of the picture?");
+	// Ask the user for the width of the picture.
+	var width = prompt("What is the width of the picture?");
 	// Calculate the area by multiplying length by width.
 	var area = length * width;
 	// Return the area results.
 	return area;
 }
 
+var a = space();
+
 var spaceArea = 375000;
 
 //This is if the user enters an empty prompt.
-if(length === "" || width === ""){
+if(length === ""){
 	//If the prompt is left blank let the user know.
 	console.log("You forgot to input something!");
-}else if(area > spaceArea){
+}else if(a > spaceArea){
 	// If the picture is larger than the area let the user know.
 	console.log("Your picture is too large!");
 }else{
@@ -25,5 +31,3 @@ if(length === "" || width === ""){
 	console.log("Your picture will fit!");
 }
 
-
-var a = space(467, 650);
