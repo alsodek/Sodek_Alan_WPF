c7390ddf8ef74dc4e0847ee70c8ccd12234c7e88
alsodek <alansodek@alans-mbp.att.net>
2014-02-27 13:05:40 -0600

    setup basic function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,17 @@
 // Alan Sodek
 // February 27, 2014
 // Functions Personal
+
+// How many bags of mulch can you fit in the trunk of your car?
+
+
+
+function space(lengh, width, height){
+	// 
+	var bags = 5;
+	var trunk = length * width * height;
+	var trunkSpace = trunk/bags;
+	return trunkSpace;
+}
+
+
